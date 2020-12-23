c4a6a9e5bfca03d9477ba05c892cf3211061fd42
Bokgil <boggil3665@daum.net>
2020-12-23 18:05:30 +0900

    clean up
    clean up

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react'
 
-import { User } from '../interfaces'
+import { User } from 'interfaces'
 
 type ListDetailProps = {
   item: User
 }
 
-const ListDetail = ({ item: user }: ListDetailProps) => (
-  <div>
-    <h1>Detail for {user.name}</h1>
-    <p>ID: {user.id}</p>
-  </div>
-)
+function onClick(name: string): void {
+  console.log('My Name is: ', name, ' !');
+}
+
+const ListDetail = ({ item: user }: ListDetailProps) => {
+  console.log(user);
+
+  return (
+    <div>
+      <h1>Detail for {user.name}</h1>
+      <p>ID: {user.id}</p>
+    </div>
+  )
+}
 
 export default ListDetail
